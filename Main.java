1ec469f27c20a2f720ef722b32ca69cc281edfa2
TE22E-Aslev-Theo <theo.aslev@elev.ga.ntig.se>
2024-04-08 09:11:13 +0200

    lagt till metoder och ett utökat menysystem

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
+import java.io.Serializable;
 import java.util.Scanner;
 
-public class Main{
+public class Main implements Serializable{
 
+    public static int[][] PlatsID =  {
+        {1,2,0,0},{5,6,7,8},{9,10,11,12},{13,14,15,16},{17,18,19,20}
+    };
+    
+    public static void setPeople(){
+        
+    }
 
     public static void PrintStartMenu(){
         // Skriver ut Start Menyn
@@ -10,6 +18,7 @@ public class Main{
                 Boka Tid[1]
             Se Lediga Platser[2]
                Avboka tid[3]      
+                  Exit[4]
         """);
         
     }
@@ -19,7 +28,31 @@ public class Main{
         
         while (true) {
             PrintStartMenu();
-            if (t.nextLine() == "hej") {
+            String answer = t.nextLine();
+
+            if (answer == "1") 
+            {
+                while (true) 
+                {
+                    
+                }
+            }
+            if (answer == "2") 
+            {
+                while (true) 
+                {
+                    
+                }
+            }
+            if (answer == "3") 
+            {
+                while (true) 
+                {
+                    
+                }
+            }
+            if (answer == "4") 
+            {
                 break;
             }
         }
